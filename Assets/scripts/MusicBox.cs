9bcaf4313b98999fcfd33b7f2199d5fe0188b27a
explosiveseccc <rdericieux25@aoiths.org>
2024-05-23 12:46:05 -0400

    help me plz :(

--- Assets/scripts/MusicBox.cs
+++ Assets/scripts/MusicBox.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MusicBox : MonoBehaviour
 {
+    private MusicVolume jukebox;
+    private AudioSource gameAudio; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jukebox = GameObject.Find("Music Settings").GetComponent<MusicVolume>();
+        gameAudio = GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameAudio.volume = jukebox.mainSlider.value;  
     }
 }
--- Assets/scripts/musicVolume.cs
+++ Assets/scripts/musicVolume.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class musicVolume : MonoBehaviour
+public class MusicVolume : MonoBehaviour
 {
     public Slider mainSlider;
 
