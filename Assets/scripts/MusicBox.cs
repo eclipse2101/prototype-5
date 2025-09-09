74c5c6bcbcc81de54c4e3b2e3542b0a15ce78739
eclipse2101 <rdericieux25@aoiths.org>
2025-09-09 14:53:54 -0400

    Fix volume slider

--- Assets/scripts/MusicBox.cs
+++ Assets/scripts/MusicBox.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicBox : MonoBehaviour
 {
-    private MusicVolume jukebox;
-    private AudioSource gameAudio; 
+    private AudioSource gameAudio;
+    private Slider musicVol; 
+    private float volume = 1;
+    bool foundJukeBox = false;
     // Start is called before the first frame update
     void Start()
-    {
-        jukebox = GameObject.Find("Music Settings").GetComponent<MusicVolume>();
+    { 
         gameAudio = GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(jukebox.mainSlider.value);
-        gameAudio.volume = jukebox.mainSlider.value;  
+        FindJukeBox();
+
+        //Debug.Log(volume);
+        gameAudio.volume = volume;  
+    }
+
+    void FindJukeBox()
+    {
+        
+            musicVol = GameObject.Find("music changer").GetComponent<Slider>();
+       
+            volume = musicVol.value;
+            foundJukeBox = true; 
     }
 }
